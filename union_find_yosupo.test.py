c31e384fb5f7c4251bd4eee8e62bb3556ddfc29e
yu-0811 <teradayu0811@gmail.com>
2024-09-22 13:37:16 +0900

    設定やり直し

---
+++ union_find_yosupo.test.py
@@ -0,0 +1,20 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+import sys
+input = sys.stdin.buffer.readline
+
+from algo_library.python.graph.unionFind import UnionFind
+
+
+def main() -> None:
+    N, Q = map(int, input().split())
+    uft = UnionFind(N)
+    for _ in range(Q):
+        t, u, v = map(int, input().split())
+        if t == 0:
+            uft.union(u, v)
+        else:
+            print(int(uft.same(u, v)))
+
+
+if __name__ == "__main__":
+    main()
