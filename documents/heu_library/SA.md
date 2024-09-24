080cc504a2f8ce5a388a62f889b008ef2da04f7f
yu-0811 <teradayu0811@gmail.com>
2024-09-24 23:58:13 +0900

    some fix

---
+++ documents/heu_library/SA.md
@@ -0,0 +1,6 @@
+---
+title: Simulated Annealing (焼きなまし法)
+documentation_of: //heu_library/SA.cpp
+---
+
+後で書く
--- tests/graph/scc_yosupo.test.py
+++ tests/graph/scc_yosupo.test.py
@@ -1,17 +1,13 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/scc
 import sys
-def input():return sys.stdin.readline().rstrip()
-def ii(): return int(input())
-def ms(): return map(int, input().split())
-def li(): return list(map(int,input().split()))
-#/////////////////////////////////
+input = sys.stdin.buffer.readline
 
 from algo_library.python.graph.scc import SCC
 
-N,M = ms()
+N,M = map(int,input().split())
 scc = SCC(N)
 for _ in range(M):
-  a,b = ms()
+  a,b = map(int,input().split())
   scc.addEdge(a,b)
 
 groups = scc.doSCC()
