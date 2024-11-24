f1dd2a0a79b3197d5d7fa8d0d0f5d154e292f9ce
yu-0811 <teradayu0811@gmail.com>
2024-11-24 09:40:52 +0900

    fix mo's algo

--- algo_library/python/data-structure/Mos_algorithm.py
+++ algo_library/python/data-structure/Mos_algorithm.py
@@ -1,84 +1,71 @@
-# 0-index で数列を扱うことに注意
+def mos_algorithm(data, queries):
+    """
+    data : list of int : 元の配列
+    queries : list of tuple : クエリのリスト
+    """
+    from math import sqrt, ceil
+    n = len(data)
+    b = ceil(sqrt(n))  # bukectのサイズ及び個数
+    # [l,r)の半開区間で考える
+    L = 0
+    R = 0
+    # queryを格納する用
+    bucket = [list() for _ in range((b + 1))]
 
-class Mo:
-    def __init__(self, data): # data は数列
-        from math import sqrt, ceil
-        self.data = data
-        self.n = len(data)
-        self.b = ceil(sqrt(self.n))  # bukectのサイズ及び個数
-        # [l,r)の半開区間で考える
-        self.l = 0
-        self.r = 0
-        # queryを格納する用
-        self.bucket = [list() for _ in range((self.b + 1))]
+    ########################################
+    # 状態を初期化する処理を書く
+    ans = 0
+    ########################################
 
-        ########################################
-        # ここに管理したい状態を初期化する処理を書く
-
-        ########################################
-
-    def _add(self, i):
+    def _add(i, ans):
         # i番目の要素を含めて考えるときへstatesを更新
         ########################################
-        pass
+
+        return ans
         ########################################
 
-    def _delete(self, i):
+    def _delete(i, ans):
         # i番目の要素を除いて考えるときへstatesを更新
         ########################################
-        pass
+
+        return ans
         ########################################
 
-    def _one_process(self, l, r):
-        # クエリ[l,r)に対してstatesを更新する
-        while self.r < r:  # rまで伸長
-            self._add(self.r)
-            self.r += 1
-        while self.r > r:  # rまで短縮
-            self.r -= 1
-            self._delete(self.r)
+    upper = len(queries)
+    upper_upper = upper * (n + 1)
+
+    for idx, (l, r) in enumerate(queries):  # queryをbucketに格納
+        bucket[l // b].append(idx + l*upper + r*upper_upper)
+
+    for i in range(len(bucket)):
+        if i%2==0: bucket[i].sort()
+        else: bucket[i].sort(reverse=True)
+
+    ret = [-1] * len(queries)
+    for b in bucket:
+        for lri in b:  # クエリに答えていく
+            i = lri % upper
+            r = lri // upper_upper
+            l = (lri % upper_upper) // upper
             
-        while self.l < l:  # lまで短縮
-            self._delete(self.l)
-            self.l += 1
-        while self.l > l:  # lまで伸長
-            self.l -= 1
-            self._add(self.l)
-
-    def process(self, queries):
-        upper = len(queries)
-        upper_upper = upper * (self.n + 1)
-
-        for idx, (l, r) in enumerate(queries):  # queryをbucketに格納
-            self.bucket[l // self.b].append(idx + l*upper + r*upper_upper)
-
-        for i in range(len(self.bucket)):
-            self.bucket[i].sort()
-
-        ret = [-1] * len(queries)
-        for b in self.bucket:
-            for lri in b:  # クエリに答えていく
-                i = lri % upper
-                r = lri // upper_upper
-                l = (lri % upper_upper) // upper
-               # クエリ[l,r)に対してstatesを更新する
-                while self.r < r:  # rまで伸長
-                    self._add(self.r)
-                    self.r += 1
-                while self.r > r:  # rまで短縮
-                    self.r -= 1
-                    self._delete(self.r)
-                    
-                while self.l < l:  # lまで短縮
-                    self._delete(self.l)
-                    self.l += 1
-                while self.l > l:  # lまで伸長
-                    self.l -= 1
-                    self._add(self.l)
+            # クエリ[l,r)に対してstatesを更新する
+            while R < r:  # rまで伸長
+                ans = _add(R,ans)
+                R += 1
+            while R > r:  # rまで短縮
+                R -= 1
+                ans = _delete(R,ans)
                 
-                ########################################
-                # クエリに答える作業をここで書く
-                # ret[i] に i 番目のクエリの答えを格納する
-                ret[i] = self.ans
-                ########################################
-        return ret
+            while L < l:  # lまで短縮
+                ans = _delete(L,ans)
+                L += 1
+            while L > l:  # lまで伸長
+                L -= 1
+                ans = _add(L,ans)
+            
+            ########################################
+            # クエリに答える作業をここで書く
+            # ret[i] に i 番目のクエリの答えを格納する
+            ret[i] = ans
+            ########################################
+    return ret
