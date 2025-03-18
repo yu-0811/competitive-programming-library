43e48169905f2c65086f720a65637375c0a3188c
yu-0811 <teradayu0811@gmail.com>
2025-03-18 19:06:00 +0900

    fix mo's algo

--- algorithm_library/python/data-structure/Mos_algorithm.py
+++ algorithm_library/python/data-structure/Mos_algorithm.py
@@ -2,70 +2,69 @@ def mos_algorithm(data, queries):
     """
     data : list of int : 元の配列
     queries : list of tuple : クエリのリスト
+    クエリは 0-indexed で半開区間[l,r)
     """
     from math import sqrt, ceil
     n = len(data)
-    b = ceil(sqrt(n))  # bukectのサイズ及び個数
+    q = len(queries)
+    B = ceil(sqrt(3)*n / (sqrt(2)*sqrt(q))) # bukectのサイズ
     # [l,r)の半開区間で考える
-    L = 0
-    R = 0
-    # queryを格納する用
-    bucket = [list() for _ in range((b + 1))]
+    L,R = 0,0
+    # queryの順番を格納する用
+    order = []
 
     ########################################
     # 状態を初期化する処理を書く
     ans = 0
+    cnt = [0]*(2*pow(10,5)+1)
     ########################################
 
     def _add(i, ans):
         # i番目の要素を含めて考えるときへstatesを更新
         ########################################
-
+        ans += (cnt[data[i]] * (cnt[data[i]]-1)) // 2
+        cnt[data[i]] += 1
         return ans
         ########################################
 
     def _delete(i, ans):
         # i番目の要素を除いて考えるときへstatesを更新
         ########################################
-
+        cnt[data[i]] -= 1
+        ans -= (cnt[data[i]] * (cnt[data[i]]-1)) // 2
         return ans
         ########################################
 
-    upper = len(queries)
-    upper_upper = upper * (n + 1)
+    # クエリの解答順
+    # (ceil(l/B), r*-1^{ceil(l/B)}) の昇順にするのが最速ぽい
+    for idx, (l,r) in enumerate(queries):
+        order.append(((l//B)<<40)+((r if (l//B)&1 else -r)<<20)+idx)
+    order.sort()
 
-    for idx, (l, r) in enumerate(queries):  # queryをbucketに格納
-        bucket[l // b].append(idx + l*upper + r*upper_upper)
-
-    for i in range(len(bucket)):
-        if i%2==0: bucket[i].sort()
-        else: bucket[i].sort(reverse=True)
-
-    ret = [-1] * len(queries)
-    for b in bucket:
-        for lri in b:  # クエリに答えていく
-            i = lri % upper
-            r = lri // upper_upper
-            l = (lri % upper_upper) // upper
+    mask = (1<<20)-1
+    ret = [-1] * Q
+    for lri in order:  # クエリに答えていく
+        i = lri & mask
+        l,r = queries[i]
+        
+        # クエリ[l,r)に対してstatesを更新する
+        while R < r:  # rまで伸長
+            ans = _add(R,ans)
+            R += 1
+        while R > r:  # rまで短縮
+            R -= 1
+            ans = _delete(R,ans)
             
-            # クエリ[l,r)に対してstatesを更新する
-            while R < r:  # rまで伸長
-                ans = _add(R,ans)
-                R += 1
-            while R > r:  # rまで短縮
-                R -= 1
-                ans = _delete(R,ans)
-                
-            while L < l:  # lまで短縮
-                ans = _delete(L,ans)
-                L += 1
-            while L > l:  # lまで伸長
-                L -= 1
-                ans = _add(L,ans)
-            
-            ########################################
-            # クエリに答える作業をここで書く
-            # ret[i] に i 番目のクエリの答えを格納する
-            ret[i] = ans
-            ########################################
+        while L < l:  # lまで短縮
+            ans = _delete(L,ans)
+            L += 1
+        while L > l:  # lまで伸長
+            L -= 1
+            ans = _add(L,ans)
+        
+        ########################################
+        # クエリに答える作業をここで書く
+        # ret[i] に i 番目のクエリの答えを格納する
+        ret[i] = ans
+        ########################################
     return ret
