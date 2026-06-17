d88b937afe78fc474f5d621f88ae998ca78592cb
yu-0811 <teradayu0811@gmail.com>
2026-06-17 22:16:09 +0900

    Add modcomb if n<

--- algorithm_library/python/math/modComb.py
+++ algorithm_library/python/math/modComb.py
@@ -1,4 +1,7 @@
-# 組合せ総数ライブラリ
+# 二項係数ライブラリ
+
+# 前計算 O(n)
+# O(1) で nCk, nHk を返す
 class Comb:
   def __init__(self,n,mod):
     self.mod = mod
@@ -21,3 +24,31 @@ class Comb:
   # 重複組合せ nHr % modを返す
   def H(self,n,r):
       return self.C(n+r-1,r)
+
+# O(k) 前計算
+# O(k) で nCk を計算（n が大きいときでも使える）
+class Comb:
+    def __init__(self, k, mod):
+        self.k = k
+        self.mod = mod
+        self.fact_inv = [0] * (k + 1)
+        self.inv = [0] * (k + 1)
+        self.fact_inv[0] = 1
+        if k >= 1:
+            self.fact_inv[1] = 1
+            self.inv[1] = 1
+
+        for i in range(2, k + 1):
+            self.inv[i] = mod - self.inv[mod % i] * (mod // i) % mod
+            self.fact_inv[i] = self.fact_inv[i - 1] * self.inv[i] % mod
+
+    def nCk(self, n, k):
+        if k < 0 or k > n:
+            return 0
+        if k > self.k:
+            raise ValueError("precomputed k is too small")
+        ans = 1
+        for i in range(k):
+            ans *= n - i
+            ans %= self.mod
+        return ans * self.fact_inv[k] % self.mod
