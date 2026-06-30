16114dec333df4074c1bcec5849e30e1f41d4cc2
yu-0811 <teradayu0811@gmail.com>
2026-07-01 00:17:02 +0900

    整理

---
+++ algorithm_library/python/techniques/Rotate.py
@@ -0,0 +1,11 @@
+def two_dimension_list_rotate_R90(L : list[list[int]]) -> list[list[int]]:
+  #2次元リストを右に90度回転させる
+  return [list(col) for col in zip(*L[::-1])]
+
+def two_dimension_list_rotate_L90(L : list[list[int]]) -> list[list[int]]:
+  #2次元リストを左に90度回転させる
+  return [list(col) for col in zip(*L)][::-1]
+
+def tention(L : list[list[int]]) -> list[list[int]]:
+  #2次元リストを転置する
+  return [list(col) for col in zip(*L)]
---
+++ algorithm_library/python/techniques/c_is_buween_a_to_b.py
@@ -0,0 +1,3 @@
+# https://atcoder.jp/contests/abc367/editorial/10712
+def c_is_buween_a_to_b(a,b,c):
+    return (a-b)*(b-c)*(c-a)>0
